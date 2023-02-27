0b9f4074fbb7693c11c8a23ebea539e17d07dae1
marvkey <marvnwadike1@gmail.com>
2023-02-27 14:54:24 -0800

    Time: 71 ms (5.04%), Space: 6 MB (14.93%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,16 +1,34 @@
-#include <algorithm>  
 class Solution {
 public:
-    bool isPalindrome(int x) {
+  bool isPalindrome(int x) {
+        int values[10];
         if(x  < 0)
             return false;
-        
-        std::string num = std::to_string(x);
-        std::string original = num;
-        std::reverse(num.begin(), num.end());
-        if(num == original)
-            return true;
-        
-        return false;
+        int back =0;
+        while(x!=0){
+            // getting the last digit in the number 
+            values[back] = x %10;
+            x/= 10;
+            back++;
+        }
+        int firstPointer= 0;
+        int secondPointer=back>0 ? back-1:0;
+      
+        // no need to check second pointer 
+        // they go at the same tieme
+        while (secondPointer!=-1){
+            std::cout<<secondPointer;
+            if(firstPointer == secondPointer){
+                if(values[firstPointer] == values[secondPointer])
+                    return true;
+            }
+            if(values[firstPointer] != values[secondPointer])
+                return false;
+            
+            firstPointer++;
+            secondPointer--;
+            
+        }
+        return true;
     }
 };
