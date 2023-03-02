36e371bfa5b33bc49026b4ac6a78709b81938be6
marvkey <marvnwadike1@gmail.com>
2023-03-02 15:02:19 -0800

    Time: 51 ms (38.16%), Space: 89.8 MB (43.20%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -6,14 +6,11 @@ public:
         combine.insert( combine.end(), nums1.begin(), nums1.end() );
         combine.insert( combine.end(), nums2.begin(), nums2.end() );
         std::sort(combine.begin(), combine.end());
-        for(auto& val : combine){
-                        std::cout<<val;
-        }
+     
         float index = combine.size();
         int indexint = index;
         if(indexint%2 ==  0.0f){
             index /=2;
-             std::cout<<endl<<combine[index-1];
             float val = combine[index-1] +combine[index];
             return val/2;
         }
